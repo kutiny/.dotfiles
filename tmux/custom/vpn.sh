46515a349f93032dfa2810316738b97f99bf729e
kutiny <alex08lta@gmail.com>
2024-07-26 17:03:46 -0300

    feat: fix vpn pill on ubuntu

--- tmux/custom/vpn.sh
+++ tmux/custom/vpn.sh
@@ -4,15 +4,14 @@ source $(dirname "$0")/tools/kit.sh
 
 showVpnStatus() {
     local vpnExecutable=~/.dotfiles/pub/scripts/vpnconnect.sh
-=
-    if [ ! -f $vpnExecutable ]; then
-        return
-    fi
-
     local tag="VPN"
     local value="󰶼"
     local on="true"
     local status=$($vpnExecutable -s | grep -E 'state|Status' | tail -n 1 | awk -F': ' '{ print $3 }')
+
+    if [[ $status ~= "No such" ]]; then
+        return
+    fi
 
     if [[ "$status" == "Disconnected" ]]; then
         on="false"
