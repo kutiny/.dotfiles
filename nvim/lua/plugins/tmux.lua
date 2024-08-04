a3acb2f99c04f0d321de7e79d99a4b12b60a91cc
kutiny <alex08lta@gmail.com>
2024-08-04 18:17:20 -0300

    chore: add develop tmux plugin

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,17 +1,32 @@
 return {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-        "TmuxNavigateLeft",
-        "TmuxNavigateDown",
-        "TmuxNavigateUp",
-        "TmuxNavigateRight",
-        "TmuxNavigatePrevious",
+    {
+        'christoomey/vim-tmux-navigator',
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
     },
-    keys = {
-        { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
+    {
+        'kutiny/tmux.nvim',
+        dev = true,
+        cmd = { 'ListSessions', },
+        enabled = false,
+        keys = {
+            { '<leader>ts', '<cmd>ListSessions<CR>' },
+        },
+        opts = {},
+        init = function()
+            -- vim.keymap.set("n", "<leader>ts", "<cmd>TmuxListSessions<CR>", {})
+        end
+    }
 }
