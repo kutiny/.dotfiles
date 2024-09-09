9c1b5258bbac1064241cf45dd5a359869e7d8550
kutiny <alex08lta@gmail.com>
2024-09-09 18:09:41 -0300

    fix: work custom

--- tmux/custom/work.sh
+++ tmux/custom/work.sh
@@ -8,33 +8,34 @@ function work {
     is_weekend=$(date +"%u" | awk '{print ($1 >= 6)}')
     tag="Free"
     is_active=true
+    value="🚀"
 
-    if [[ $is_worktime && !$is_weekend ]]; then
+    if [[ "$is_worktime" == "1" && "$is_weekend" == "0" ]]; then
         is_active=false
         tag="Working"
+        # get remaining time until 18:00hs in human format and in shell code (macos)
+        rem_time=$(date -j -f "%H:%M:%S" "$(date +"%T")" "18:00:00" +%S)
+
+        # Get the current date and time in seconds since epoch
+        current_time=$(date +%s)
+
+        # Get today's date with 18:00 time in seconds since epoch
+        target_time=$(date -j -f "%Y-%m-%d %H:%M:%S" "$(date +'%Y-%m-%d') 18:00:00" +%s)
+
+        # Calculate the remaining seconds
+        remaining_seconds=$((target_time - current_time))
+
+        # Convert remaining seconds to hours, minutes, and seconds
+        hours=$((remaining_seconds / 3600))
+        minutes=$(((remaining_seconds % 3600) / 60))
+
+        # Add leading zeros to hours, minutes and seconds
+        hours=$(printf "%02d" $hours)
+        minutes=$(printf "%02d" $minutes)
+        value="$hours:$minutes"
     fi
 
-    # get remaining time until 18:00hs in human format and in shell code (macos)
-    rem_time=$(date -j -f "%H:%M:%S" "$(date +"%T")" "18:00:00" +%S)
-
-    # Get the current date and time in seconds since epoch
-    current_time=$(date +%s)
-
-    # Get today's date with 18:00 time in seconds since epoch
-    target_time=$(date -j -f "%Y-%m-%d %H:%M:%S" "$(date +'%Y-%m-%d') 18:00:00" +%s)
-
-    # Calculate the remaining seconds
-    remaining_seconds=$((target_time - current_time))
-
-    # Convert remaining seconds to hours, minutes, and seconds
-    hours=$((remaining_seconds / 3600))
-    minutes=$(((remaining_seconds % 3600) / 60))
-
-    # Add leading zeros to hours, minutes and seconds
-    hours=$(printf "%02d" $hours)
-    minutes=$(printf "%02d" $minutes)
-
-    print_pill $tag "$hours:$minutes" $is_active
+    print_pill $tag "$value" $is_active
 }
 
 work
