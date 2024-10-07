322b9eeada4b9be83d9bdc34f36d52e4560fe71a
kutiny <alex08lta@gmail.com>
2024-10-07 10:00:24 -0300

    chore: add keys for worktrees

--- nvim/lua/plugins/worktree.lua
+++ nvim/lua/plugins/worktree.lua
@@ -1,13 +1,19 @@
 return {
-    'ThePrimeagen/git-worktree.nvim',
-    dev = true,
+    'kutiny/git-worktree.nvim',
+    dev = false,
     config = function()
         local worktree = require('git-worktree');
         worktree.on_tree_change(function(op, metadata)
             if op == worktree.Operations.Switch then
-                print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
                 require('oil').open(metadata.path)
             end
         end)
     end,
+    keys = {
+        { '<leader>wl', ":lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", { silent = true } },
+        -- { '<leader>ws', ':lua require("git-worktree").create()<CR>' },
+        -- { '<leader>wc', ':lua require("git-worktree").create(true)<CR>' },
+        -- { '<leader>wd', ':lua require("git-worktree").delete()<CR>' },
+        -- { '<leader>ww', ':lua require("git-worktree").switch()<CR>' },
+    }
 }
