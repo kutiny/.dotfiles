3e24f96cb02d6e05b9c4962a15c65be2cbdba406
kutiny <alex08lta@gmail.com>
2025-02-06 15:01:24 -0300

    feat: add witch

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -291,4 +291,16 @@ return {
         end,
         name = 'rose-pine',
     },
+    {
+        "sontungexpt/witch",
+        priority = 1000,
+        lazy = true,
+        event = 'UIEnter',
+        opts = {
+
+        },
+        -- config = function(_, opts)
+        --     require("witch").setup(opts)
+        -- end,
+    },
 }
