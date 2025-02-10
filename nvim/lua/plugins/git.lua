e9d8c67c457b697fa63f9ae96e52f27d6185bf0d
kutiny <alex08lta@gmail.com>
2025-02-10 12:50:00 -0300

    feat: add keys to gitsigns

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -15,5 +15,10 @@ return {
         opts = {},
         event = { 'BufReadPre', 'BufNewFile' },
         ft = { '!dashboard' },
+        keys = {
+            { "<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", desc = "Git preview hunk" },
+            { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>", desc = "Git prev hunk" },
+            { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>", desc = "Git next hunk" },
+        },
     },
 }
