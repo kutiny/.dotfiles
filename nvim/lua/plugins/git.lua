e431e5741865a43695994684aa58dae60f44728a
kutiny <alex08lta@gmail.com>
2025-02-10 17:06:20 -0300

    chore: update keys on git plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -5,6 +5,9 @@ return {
         opts = {
             enabled = false,
             delay = 100,
+            message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+            date_format = "%d %b %Y %H:%M:%S (%r)", -- template for the date, check Date format section for more options
+            virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
         },
         keys = {
             { "<leader>bb", "<cmd>GitBlameToggle<CR>", desc = "Blame toggle" },
@@ -17,8 +20,9 @@ return {
         ft = { '!dashboard' },
         keys = {
             { "<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", desc = "Git preview hunk" },
-            { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>", desc = "Git prev hunk" },
-            { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>", desc = "Git next hunk" },
+            { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",    desc = "Git prev hunk" },
+            { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>",    desc = "Git next hunk" },
+            { "<leader>bl", "<cmd>Gitsigns blame_line<CR>",   desc = "Git blame line" },
         },
     },
 }
