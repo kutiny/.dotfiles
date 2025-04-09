18db0c666f1f2a9821bd368b7dea0d0af78288d1
kutiny <alex08lta@gmail.com>
2025-04-09 15:43:55 -0300

    chore(oil): configure float to left

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,9 +4,9 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = { "Oil", },
     keys = {
-        { "<leader>pv", "<cmd>lua require'oil'.toggle_float()<CR>",                                         { silent = true } },
+        { "<leader>pv", "<cmd>lua require'oil'.open(vim.fn.getcwd())<CR>",      { silent = true } },
         { "<leader>pr", "<cmd>lua require'oil.actions'.refresh.callback()<CR>", { silent = true } },
-        { "<leader>ph", "<cmd>lua require'oil'.open(vim.fn.getcwd())<CR>", { silent = true } },
+        { "<leader>pf", "<cmd>lua require'oil'.toggle_float()<CR>",             { silent = true } },
     },
     opts = {
         -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
@@ -128,8 +128,8 @@ return {
             -- This is the config that will be passed to nvim_open_win.
             -- Change values here to customize the layout
             override = function(conf)
-                conf.row = -1
-                conf.col = -1
+                conf.row = 0
+                conf.col = 0
                 return conf
             end,
         },
