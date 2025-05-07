837ab0c4fdc2724e9b04046ef39ad265c3b2ea62
kutiny <alex08lta@gmail.com>
2025-05-07 17:11:02 -0300

    chore(nvim): make resize greater

--- nvim/lua/kutiny/custom.lua
+++ nvim/lua/kutiny/custom.lua
@@ -7,3 +7,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank({ visual = true, timeout = 200 })
     end,
 })
+
+vim.api.nvim_create_user_command("OpenFolder", function()
+    local path = vim.fn.expand("%:p:h")
+    if not vim.fn.isdirectory(path) == 1 then
+        path = path:gsub("Oil://", "")
+    end
+    vim.cmd("silent !open " .. path)
+end, { desc = "Open the folder of the current file" })
+
+vim.keymap.set("n", "<C-w><", "<CMD>vertical resize -15<CR>")
+vim.keymap.set("n", "<C-w>>", "<CMD>vertical resize +15<CR>")
+vim.keymap.set("n", "<C-w>+", "<CMD>resize +5<CR>")
+vim.keymap.set("n", "<C-w>-", "<CMD>resize -5<CR>")
+
