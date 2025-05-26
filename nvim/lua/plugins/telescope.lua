17facefdabc13bb564ae58c617e49a6955cce706
kutiny <alex08lta@gmail.com>
2025-05-25 21:33:08 -0300

    feat: add grep args to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
     'nvim-telescope/telescope.nvim',
     version = '0.1.8',
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
+    dependencies = {
+        { 'nvim-lua/plenary.nvim' },
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim",
+            version = "^1.0.0",
+        },
+    },
     cmd = { 'Telescope' },
     keys = {
         { '<leader>ps', function()
@@ -32,6 +38,7 @@ return {
         end },
     },
     config = function()
+        local lga_actions = require("telescope-live-grep-args.actions")
         require('telescope').setup({
             defaults = {
                 -- NOTE: this is the only thing that works for hidden files search in rp
@@ -93,8 +100,26 @@ return {
                     initial_mode = "normal",
                 },
             },
-            extensions = {},
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true, -- enable/disable auto-quoting
+                    -- define mappings, e.g.
+                    mappings = { -- extend mappings
+                        i = {
+                            ["<C-k>"] = lga_actions.quote_prompt(),
+                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                            -- freeze the current list and start a fuzzy search in the frozen list
+                            ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                        },
+                    },
+                    -- ... also accepts theme settings, for example:
+                    -- theme = "dropdown", -- use dropdown theme
+                    -- theme = { }, -- use own theme spec
+                    -- layout_config = { mirror=true }, -- mirror preview pane
+                }
+            },
         })
         require("telescope").load_extension("git_worktree")
+        require("telescope").load_extension("live_grep_args")
     end
 }
