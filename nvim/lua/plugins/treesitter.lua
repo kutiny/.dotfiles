d1e900b7f383040ce782111335c49b78d4818020
kutiny <alex08lta@gmail.com>
2026-02-14 12:48:17 -0300

    chore: update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'main',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
         require 'nvim-treesitter.config'.setup {
--- shell/manual-buildx.sh
+++ shell/manual-buildx.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-alias buildx=~/.docker/cli-plugins/buildx-v0.27.0.darwin-arm64
+alias buildx=~/.docker/cli-plugins/buildx-v0.30.1.darwin-arm64
