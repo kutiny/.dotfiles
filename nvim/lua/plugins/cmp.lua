277ce42631f16b8e14be8765b5ce001b4e3d1202
kutiny <alex08lta@gmail.com>
2026-06-09 09:21:06 -0300

    feat: update nvim plugins

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,98 +1,144 @@
 return {
-    -- {
-    --     'zbirenbaum/copilot.lua',
-    --     lazy = true,
-    --     cmd = { 'Copilot' },
-    --     opts = { panel = { enabled = false }, suggestion = { enabled = false } },
-    --     event = { 'InsertEnter', 'LspAttach', 'BufReadPre', 'BufNewFile' },
-    -- },
-    -- {
-    --     'zbirenbaum/copilot-cmp',
-    --     event = { 'InsertEnter', 'LspAttach', 'BufReadPre', 'BufNewFile' },
-    --     lazy = true,
-    --     dependencies = { 'zbirenbaum/copilot.lua' },
-    --     config = function()
-    --         require('copilot_cmp').setup({
-    --             fix_pairs = true
-    --         })
-    --     end
-    -- },
-    -- {
-    --     'hrsh7th/nvim-cmp',
-    --     event = { 'InsertEnter' },
-    --     dependencies = {
-    --         'L3MON4D3/LuaSnip',
-    --         'hrsh7th/cmp-nvim-lsp',
-    --         'hrsh7th/cmp-path',
-    --         'hrsh7th/cmp-buffer',
-    --         'saadparwaiz1/cmp_luasnip',
-    --         'onsails/lspkind.nvim',
-    --     },
-    --     config = function()
-    --         vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-    --
-    --         local cmp = require('cmp')
-    --         local lspkind = require('lspkind')
-    --
-    --         cmp.setup({
-    --             snippet = {
-    --                 expand = function(args)
-    --                     require('luasnip').lsp_expand(args.body)
-    --                 end,
-    --             },
-    --             window = {
-    --                 -- completion = cmp.config.window.bordered(),
-    --                 -- documentation = cmp.config.window.bordered(),
-    --             },
-    --             mapping = cmp.mapping.preset.insert({
-    --                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    --                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    --                 ['<C-Space>'] = cmp.mapping.complete(),
-    --                 ['<C-e>'] = cmp.mapping.abort(),
-    --                 ['<CR>'] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    --             }),
-    --             sources = cmp.config.sources({
-    --                 { name = 'nvim_lsp', max_item_count = 20 },
-    --                 { name = 'luasnip',  max_item_count = 10 },
-    --             }, {
-    --                 { name = 'buffer', max_item_count = 10 },
-    --                 { name = 'copilot',  max_item_count = 3 },
-    --             }),
-    --             formatting = {
-    --                 format = lspkind.cmp_format({
-    --                     mode = "symbol",
-    --                     max_width = 50,
-    --                     symbol_map = {
-    --                         Copilot = "",
-    --                         Text = "󰉿",
-    --                         Method = "󰆧",
-    --                         Function = "󰊕",
-    --                         Constructor = "",
-    --                         Field = "󰜢",
-    --                         Variable = "󰀫",
-    --                         Class = "󰠱",
-    --                         Interface = "",
-    --                         Module = "",
-    --                         Property = "󰜢",
-    --                         Unit = "󰑭",
-    --                         Value = "󰎠",
-    --                         Enum = "",
-    --                         Keyword = "󰌋",
-    --                         Snippet = "",
-    --                         Color = "󰏘",
-    --                         File = "󰈙",
-    --                         Reference = "󰈇",
-    --                         Folder = "󰉋",
-    --                         EnumMember = "",
-    --                         Constant = "󰏿",
-    --                         Struct = "󰙅",
-    --                         Event = "",
-    --                         Operator = "󰆕",
-    --                         TypeParameter = "",
-    --                     }
-    --                 })
-    --             },
-    --         })
-    --     end
-    -- }
+    {
+        'zbirenbaum/copilot.lua',
+        lazy = true,
+        cmd = { 'Copilot' },
+        opts = { panel = { enabled = false }, suggestion = { enabled = false } },
+        event = { 'InsertEnter', 'LspAttach', 'BufReadPre', 'BufNewFile' },
+    },
+    {
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets', 'zbirenbaum/copilot.lua', 'giuxtaposition/blink-cmp-copilot' },
+        version = '1.*',
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = {
+                preset = 'default',
+                ['<C-n>'] = {
+                    function(cmp)
+                        if not cmp.is_menu_visible() then
+                            cmp.show()
+                            return true
+                        end
+                    end,
+                    'select_next',
+                }
+            },
+
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono',
+                -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+                kind_icons = {
+                    Copilot = "",
+                    Text = '󰉿',
+                    Method = '󰊕',
+                    Function = '󰊕',
+                    Constructor = '󰒓',
+
+                    Field = '󰜢',
+                    Variable = '󰆦',
+                    Property = '󰖷',
+
+                    Class = '󱡠',
+                    Interface = '󱡠',
+                    Struct = '󱡠',
+                    Module = '󰅩',
+
+                    Unit = '󰪚',
+                    Value = '󰦨',
+                    Enum = '󰦨',
+                    EnumMember = '󰦨',
+
+                    Keyword = '󰻾',
+                    Constant = '󰏿',
+
+                    Snippet = '󱄽',
+                    Color = '󰏘',
+                    File = '󰈔',
+                    Reference = '󰬲',
+                    Folder = '󰉋',
+                    Event = '󱐋',
+                    Operator = '󰪚',
+                    TypeParameter = '󰬛',
+                },
+            },
+
+            signature = {
+                window = {
+                    border = 'rounded'
+                }
+            },
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = {
+                documentation = {
+                    auto_show = false,
+                    window = {
+                        border = 'rounded',
+                        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+                    }
+                },
+                ghost_text = { enabled = true, show_with_menu = true },
+                list = {
+                    selection = {
+                        preselect = false,
+                        auto_insert = false,
+                    }
+                },
+
+                menu = {
+                    border = 'rounded',
+                    auto_show = false,
+                    draw = {
+                        columns = { { "label", "label_description", gap = 1 }, { "kind_icon" }, { "kind" }, { "source_name" } },
+                    }
+                }
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = {'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+                providers = {
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                        score_offset = 100,
+                        async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = "Copilot"
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
+                    },
+                },
+            },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    }
 }
